2ef7b7672585834516f3f71a8144a8ca5558896b
John Paul Fababaer <100891759+jpfababaer@users.noreply.github.com>
2024-02-14 16:52:56 +0000

    finished the exercise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,28 @@ class UsersController < ApplicationController
     render({template: "users_template/show"})
   end
 
+  def create
+    new_user = User.new
+
+    new_user.username = params.fetch("username_info")
+
+    new_user.save
+
+    redirect_to("/users/#{new_user.username}")
+  end
+
+  def update
+    user_id = params.fetch("path_id")
+    
+    matching_user = User.where({ id: user_id})
+    @the_user = matching_user.at(0)
+
+    @the_user.username = params.fetch("username_info")
+
+    @the_user.save
+
+    redirect_to("/users/#{@the_user.username}")
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   #10 Conventional naming: name the controller after the Table - plural = users. When name an action that LISTS things -> "index"
   get("/users", {controller: "users", action: "index"})
   get("/users/:path_username", {controller: "users", action: "show"})
+  post("/add_user", controller: "users", action: "create")
+  post("/update_user/:path_id", controller: "users", action: "update")
 
   #-----------------------------------------
   #Photos page:
